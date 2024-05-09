7e69ffaa6380bf26bb63e7fef2245bfe6ece8448
Vincenzo Piccinonna <vincentbibo@gmail.com>
2024-05-09 21:09:17 +0200

    continuazione esercizio

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
+Route::get('/chi-siamo', function () {
+    return view('aboutus');
+})->name('aboutus');
+Route::get('/servizi',
+ function () {
+    $servizi = [
+        ['name' => 'Siti Web', 'uri' => 'siti-web'],
+        ['name' => 'Negozi Digitali', 'uri' => 'ecommerce'],
+        ['name' => 'Gestionali in Cloud', 'uri' => 'gestionali-cloud'],
+        ['name' => 'Marketing Digitale', 'uri' => 'marketing-digitale'],
+    ];
+
+
+
+    return view('service',["servizi"=>$servizi]); 
+})->name('service');
+Route::get('/dettaglio-servizo/{service}', function ($service) {
+    return view('detail',['service'=>$service]);
+})->name('detail');
+
